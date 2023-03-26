422e5b2c2aebeafe01655815a738f69f8414549a
isam <isamelb@gmail.com>
2023-03-26 19:20:41 +0100

    updated to latest code

--- api/index.py
+++ api/index.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from googletrans import Translator
+from googletrans import Translator, LANGUAGES
+import traceback
+
+def get_translator():
+    return Translator(service_urls=['translate.google.com'])
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="api/static"), name="static")
 templates = Jinja2Templates(directory="api/templates")
-translator = Translator()
+translator = get_translator()
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
@@ -15,7 +19,11 @@ async def home(request: Request):
 
 @app.post("/translate")
 async def translate(request: Request):
-    data = await request.form()
-    text = data["text"]
-    translated_text = translator.translate(text, src='en', dest='fr').text
-    return {"translated_text": translated_text}
+    try:
+        data = await request.form()
+        text = data["text"]
+        translated_text = translator.translate(text, src='en', dest='fr').text
+        return {"translated_text": translated_text}
+    except Exception as e:
+        print(traceback.format_exc())
+        return {"error": str(e)}
